4f8409eee6fa6292ccef06893c602941658a2d65
Alex <iavman@inbox.ru>
2023-05-17 13:39:18 +0300

    init database

--- CommBank-Server/Program.cs
+++ CommBank-Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CommBank.Models;
 using CommBank.Services;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,38 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("
 
 var mongoClient = new MongoClient(builder.Configuration.GetConnectionString("CommBank"));
 var mongoDatabase = mongoClient.GetDatabase("CommBank");
+
+var collections = await mongoDatabase.ListCollectionsAsync();
+
+if (collections.Any() == false)
+{
+    var collectionUsers = mongoDatabase.GetCollection<User>("Users");
+    string textUsers = File.ReadAllText(@"Data/Users.json");
+    var users = BsonSerializer.Deserialize<List<User>>(textUsers);
+    await collectionUsers.InsertManyAsync(users);
+
+    var collectionTransactions = mongoDatabase.GetCollection<CommBank.Models.Transaction>("Transactions");
+    string textTransactions = File.ReadAllText(@"Data/Transactions.json");
+    var transactions = BsonSerializer.Deserialize<List<CommBank.Models.Transaction>>(textTransactions);
+    await collectionTransactions.InsertManyAsync(transactions);
+
+    var collectionTags = mongoDatabase.GetCollection<CommBank.Models.Tag>("Tags");
+    string textTags = File.ReadAllText(@"Data/Tags.json");
+    var tags = BsonSerializer.Deserialize<List<CommBank.Models.Tag>>(textTags);
+    await collectionTags.InsertManyAsync(tags);
+
+    var collectionGoals = mongoDatabase.GetCollection<Goal>("Goals");
+    string textGoals = File.ReadAllText(@"Data/Goals.json");
+    var goals = BsonSerializer.Deserialize<List<Goal>>(textGoals);
+    await collectionGoals.InsertManyAsync(goals);
+
+    var collectionAccounts = mongoDatabase.GetCollection<Account>("Accounts");
+    string textAccounts = File.ReadAllText(@"Data/Accounts.json");
+    var accounts = BsonSerializer.Deserialize<List<Account>>(textAccounts);
+    await collectionAccounts.InsertManyAsync(accounts);
+}
+
+
 
 IAccountsService accountsService = new AccountsService(mongoDatabase);
 IAuthService authService = new AuthService(mongoDatabase);
